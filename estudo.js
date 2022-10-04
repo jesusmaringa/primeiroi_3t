da2a5cad87373865da9d4fe82f559c53a99b385b
Jesus <101044903+jesusmaringa@users.noreply.github.com>
2022-10-04 17:42:53 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -15,3 +15,19 @@ function alo(){
     document.write(tabuada + " x 9 = " + (tabuada*9)+"<br>");
     document.write(tabuada + " x 10 = " + (tabuada*10));
 }
+// for(inicio, validaçao, manutençao)
+function escreva(){
+    for(var i=1; i <= 10; i++){
+        for(var j = 1; j <=10; j++){
+            document.write(i + " x " + j + " = " + (j*i)+"<br>");
+        }
+        document.write("<br>");
+    }
+}
+
+function quadrado(){
+    for(var i = 2; i < 11; i++){
+        document.write("O quadrado de " + i + " é " + (i*i)+"<br>");
+    }
+}
+
