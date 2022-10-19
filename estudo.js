e844d4b4be1c84729a75e5eadd140c9b60dc105c
Jesus <101044903+jesusmaringa@users.noreply.github.com>
2022-10-19 17:30:40 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -37,6 +37,13 @@ function quadrado(){
 function total(){
     let val = document.getElementById("valor").value;
     let ju = document.getElementById("juros").value;
-    let resultado = (val * ((ju/100)+1));
+    
+    let t = document.getElementById("meses").value;
+    let resultado = 0;
+    for(let m = 1; m < t; m++){
+        resultado = (val * ((ju/100)+1));
+        val = resultado;
+        document.write("Mês " + m + " valor de " + val + "<br>");
+    }
     document.write("O total é de: " + resultado);
 }
