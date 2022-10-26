6f66f455efe9f71353587fa474398505dd4bc196
Jesus <101044903+jesusmaringa@users.noreply.github.com>
2022-10-26 17:40:25 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -42,10 +42,14 @@ function total(){
     
     let t = document.getElementById("meses").value;
     let resultado = 0;
+    let saida = "";
     for(let m = 1; m <= t; m++){
         resultado = (val * ((ju/100)+1));
         val = resultado;
-        document.write("Mês " + m + " valor de " + moeda(val) + "<br>");
+         saida += "Mês " + m + ": " + moeda(val) + "<br>";
+       //document.write("Mês " + m + " valor de " + moeda(val) + "<br>");
     }
-    document.write("O total é de: " + moeda(resultado) );
+    document.getElementById("mes").innerHTML = saida;
+    document.getElementById("resultado").innerHTML = "Total: " + moeda(resultado);
+    // document.write("O total é de: " + moeda(resultado) );
 }
